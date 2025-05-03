5c78910fe5d54919d749ab6b0caf254c5c0923f8
quiklynmx <abdulsamii247@gmail.com>
2025-05-03 06:39:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// Simple scroll to top on header button click
+// Smooth Scroll for Navigation Links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Scroll to About Section on Button Click
 document.querySelector('.btn').addEventListener('click', function() {
   document.querySelector('#about').scrollIntoView({
     behavior: 'smooth'
