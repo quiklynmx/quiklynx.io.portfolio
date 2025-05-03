35a5bdedd193a60bf27bf3e2278fd38add3acd0d
quiklynmx <abdulsamii247@gmail.com>
2025-05-03 06:33:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// Optional: You can add smooth scroll or other JS effects here
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+// Simple scroll to top on header button click
+document.querySelector('.btn').addEventListener('click', function() {
+  document.querySelector('#about').scrollIntoView({
+    behavior: 'smooth'
   });
 });
